091dc9d7b5fcf7b3d182d3291fc468a8a01115ee
Diego Silva <83185661+diegossilva22@users.noreply.github.com>
2024-03-06 21:31:38 -0300

    Update SOMA.java

--- SOMA.java
+++ SOMA.java
@@ -1,1 +1,16 @@
+package domain;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("VALOR DA VARIÁVEL SOMA");
+        int indice = 13;
+        int soma = 0;
+        int k = 0;
+        while(k<indice){
+            k = k + 1;
+            soma = soma + k;
+        }System.out.println("Soma: " + soma);
+    }
+}
 
